df284cb4736c0969277ca19f236036fba10a4884
Ariful Hasan <arifhasan.mbstu@gmail.com>
2026-01-15 21:33:45 +0100

    add schedule in kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('app:generate-short-code')
+        ->everyFiveMinutes()
+        ->withoutOverlapping(10);
     }
 
     /**
